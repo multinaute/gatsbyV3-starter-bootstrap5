d8df5bb930beccf0d7fbb54158aabee76bc7e4e2
Söw Yet <multinaute@gmail.com>
2021-10-17 17:56:58 +0200

    fix add file

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { FaMap } from "react-icons/fa"
+
+function About() {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+        <FaMap className="me-2" />
+        Primary button
+      </button>
+    </div>
+  )
+}
+
+export default About
